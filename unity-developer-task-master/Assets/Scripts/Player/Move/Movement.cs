9169136090b515c3220f269e67d0653f5722f373
erproxy <69542801+erproxy@users.noreply.github.com>
2021-07-07 09:19:14 +0400

    Movement
    Реализовал логику передвижения

---
+++ unity-developer-task-master/Assets/Scripts/Player/Move/Movement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{ 
+    
+    [Header("Movement settings")]
+    [SerializeField] private float _speed;
+
+    private Rigidbody _rb;
+    
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        MoveLogic();
+    }
+    
+    private void MoveLogic()
+    {
+        _rb.AddForce(_movementVector * _speed, ForceMode.Impulse);
+    }
+    
+    private Vector3 _movementVector
+    {
+        get
+        {
+            var horizontal = Input.GetAxis("Horizontal");
+            var vertical = Input.GetAxis("Vertical");
+
+            return new Vector3(horizontal, 0.0f, vertical);
+        }
+    }
+
+
+
+
+}
